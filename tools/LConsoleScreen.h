9f1138565e58fa08ee13e0dd1d1736ad778b0eac
LioQing <s2010157@gmail.com>
2020-06-14 17:27:35 +0800

    INDENTATION & customizable title

--- tools/LConsoleScreen.h
+++ tools/LConsoleScreen.h
@@ -23,38 +23,38 @@ lpConsoleCurrentFontEx);
 
 enum COLOUR
 {
-	FG_BLACK		= 0x0000,
+	FG_BLACK        = 0x0000,
 	FG_DARK_BLUE    = 0x0001,	
 	FG_DARK_GREEN   = 0x0002,
 	FG_DARK_CYAN    = 0x0003,
 	FG_DARK_RED     = 0x0004,
 	FG_DARK_MAGENTA = 0x0005,
 	FG_DARK_YELLOW  = 0x0006,
-	FG_GREY			= 0x0007,
+	FG_GREY         = 0x0007,
 	FG_DARK_GREY    = 0x0008,
-	FG_BLUE			= 0x0009,
+	FG_BLUE         = 0x0009,
 	FG_GREEN		= 0x000A,
 	FG_CYAN			= 0x000B,
-	FG_RED			= 0x000C,
-	FG_MAGENTA		= 0x000D,
-	FG_YELLOW		= 0x000E,
-	FG_WHITE		= 0x000F,
-	BG_BLACK		= 0x0000,
-	BG_DARK_BLUE	= 0x0010,
-	BG_DARK_GREEN	= 0x0020,
-	BG_DARK_CYAN	= 0x0030,
-	BG_DARK_RED		= 0x0040,
+	FG_RED          = 0x000C,
+	FG_MAGENTA      = 0x000D,
+	FG_YELLOW       = 0x000E,
+	FG_WHITE        = 0x000F,
+	BG_BLACK        = 0x0000,
+	BG_DARK_BLUE    = 0x0010,
+	BG_DARK_GREEN   = 0x0020,
+	BG_DARK_CYAN    = 0x0030,
+	BG_DARK_RED     = 0x0040,
 	BG_DARK_MAGENTA = 0x0050,
-	BG_DARK_YELLOW	= 0x0060,
-	BG_GREY			= 0x0070,
-	BG_DARK_GREY	= 0x0080,
-	BG_BLUE			= 0x0090,
-	BG_GREEN		= 0x00A0,
-	BG_CYAN			= 0x00B0,
-	BG_RED			= 0x00C0,
-	BG_MAGENTA		= 0x00D0,
-	BG_YELLOW		= 0x00E0,
-	BG_WHITE		= 0x00F0,
+	BG_DARK_YELLOW  = 0x0060,
+	BG_GREY         = 0x0070,
+	BG_DARK_GREY    = 0x0080,
+	BG_BLUE         = 0x0090,
+	BG_GREEN        = 0x00A0,
+	BG_CYAN         = 0x00B0,
+	BG_RED          = 0x00C0,
+	BG_MAGENTA      = 0x00D0,
+	BG_YELLOW       = 0x00E0,
+	BG_WHITE        = 0x00F0,
 };
 
 enum PIXEL_TYPE
@@ -76,13 +76,13 @@ public:
         m_rHnd = GetStdHandle(STD_INPUT_HANDLE);
     }
 
-    void Init(int width, int height, int fontw = 8, int fonth = 16)
+    void Init(int width, int height, int fontw = 8, int fonth = 16, const char* title = "LConsoleScreen")
     {
         // set size
         m_size = { (short)(width - 1), (short)(height - 1) };
 
         //set title
-        SetConsoleTitle(TEXT("TEST"));
+        SetConsoleTitle(LPCSTR(title));
 
         // set windows size
         SMALL_RECT win_size = { 0, 0, m_size.X, m_size.Y };
@@ -93,12 +93,12 @@ public:
 
         // set font info
         CONSOLE_FONT_INFOEX cfi;
-		cfi.cbSize = sizeof(cfi);
-		cfi.nFont = 0;
-		cfi.dwFontSize.X = fontw;
-		cfi.dwFontSize.Y = fonth;
-		cfi.FontFamily = FF_DONTCARE;
-		cfi.FontWeight = FW_NORMAL;
+        cfi.cbSize = sizeof(cfi);
+        cfi.nFont = 0;
+        cfi.dwFontSize.X = fontw;
+        cfi.dwFontSize.Y = fonth;
+        cfi.FontFamily = FF_DONTCARE;
+        cfi.FontWeight = FW_NORMAL;
 
         wcsncpy(cfi.FaceName, L"Consolas", LF_FACESIZE);
         SetCurrentConsoleFontEx(m_wHnd, false, &cfi);
