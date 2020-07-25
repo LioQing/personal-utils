1637001b9a19b033d06430ad3b52982b00cbd7a9
LioQing <46854695+LioQing@users.noreply.github.com>
2020-07-25 19:26:00 +0800

    namespace lio for everything that don't have namspace

--- data structures/Matrix.hpp
+++ data structures/Matrix.hpp
@@ -3,98 +3,101 @@
 #include <cstdint>
 #include <vector>
 
-template <typename T>
-class Matrix
+namespace lio
 {
-public:
-
-	std::vector<std::vector<T>> matrix;
-
-	uint32_t width, height;
-
-	Matrix(std::vector<std::vector<T>> matrix) : matrix(matrix)
+	template <typename T>
+	class Matrix
 	{
-		width = matrix.at(0).size();
-		height = matrix.size();
-	}
+	public:
 
-	Matrix(uint32_t width, uint32_t height, T val) : width(width), height(height)
-	{
-		matrix.resize(height);
-		for (auto& h : matrix) h.resize(width);
+		std::vector<std::vector<T>> matrix;
 
-		Fill(val);
-	}
+		uint32_t width, height;
 
-	Matrix& operator=(const Matrix& m)
-	{
-		height = m.height;
-		width = m.width;
-
-		matrix.resize(height);
-		for (auto& h : matrix) h.resize(width);
-
-		for (auto h = 0u; h < height; ++h)
+		Matrix(std::vector<std::vector<T>> matrix) : matrix(matrix)
 		{
-			for (auto w = 0u; w < width; ++w)
-			{
-				At(w, h) = m.At(w, h);
-			}
+			width = matrix.at(0).size();
+			height = matrix.size();
 		}
 
-		return *this;
-	}
-
-	void SetSize(uint32_t w, uint32_t h, T val)
-	{
-		matrix.resize(h);
-		for (auto& h : matrix) h.resize(w);
-
-		for (auto tmp_h = 0u; tmp_h < h; ++tmp_h)
+		Matrix(uint32_t width, uint32_t height, T val) : width(width), height(height)
 		{
-			for (auto tmp_w = 0u; tmp_w < w; ++tmp_w)
-			{
-				if (tmp_h < height || tmp_w < width) continue;
-				At(tmp_w, tmp_h) = val;
-			}
+			matrix.resize(height);
+			for (auto& h : matrix) h.resize(width);
+
+			Fill(val);
 		}
 
-		width = w;
-		height = h;
-	}
-
-	std::vector<T>& Row(uint32_t r)
-	{
-		return matrix.at(r);
-	}
-	// to not affect the performance, returning pointers is necessary
-	std::vector<T*> Column(uint32_t c)
-	{
-		std::vector<T*> col;
-		for (auto h = 0u; h < height; ++h) col.push_back(&matrix.at(h).at(c));
-		return col;
-	}
-
-	T& At(uint32_t x, uint32_t y)
-	{
-		return matrix.at(y).at(x);
-	}
-	T At(uint32_t x, uint32_t y) const
-	{
-		return matrix.at(y).at(x);
-	}
-
-	void Fill(T val)
-	{
-		for (auto& h : matrix)
+		Matrix& operator=(const Matrix& m)
 		{
-			for (auto& w : h)
+			height = m.height;
+			width = m.width;
+
+			matrix.resize(height);
+			for (auto& h : matrix) h.resize(width);
+
+			for (auto h = 0u; h < height; ++h)
 			{
-				w = val;
+				for (auto w = 0u; w < width; ++w)
+				{
+					At(w, h) = m.At(w, h);
+				}
+			}
+
+			return *this;
+		}
+
+		void SetSize(uint32_t w, uint32_t h, T val)
+		{
+			matrix.resize(h);
+			for (auto& h : matrix) h.resize(w);
+
+			for (auto tmp_h = 0u; tmp_h < h; ++tmp_h)
+			{
+				for (auto tmp_w = 0u; tmp_w < w; ++tmp_w)
+				{
+					if (tmp_h < height || tmp_w < width) continue;
+					At(tmp_w, tmp_h) = val;
+				}
+			}
+
+			width = w;
+			height = h;
+		}
+
+		std::vector<T>& Row(uint32_t r)
+		{
+			return matrix.at(r);
+		}
+		// to not affect the performance, returning pointers is necessary
+		std::vector<T*> Column(uint32_t c)
+		{
+			std::vector<T*> col;
+			for (auto h = 0u; h < height; ++h) col.push_back(&matrix.at(h).at(c));
+			return col;
+		}
+
+		T& At(uint32_t x, uint32_t y)
+		{
+			return matrix.at(y).at(x);
+		}
+		T At(uint32_t x, uint32_t y) const
+		{
+			return matrix.at(y).at(x);
+		}
+
+		void Fill(T val)
+		{
+			for (auto& h : matrix)
+			{
+				for (auto& w : h)
+				{
+					w = val;
+				}
 			}
 		}
-	}
-};
+	};
 
-typedef Matrix<int> Matrixi;
-typedef Matrix<float> Matrixf;
+	typedef Matrix<int> Matrixi;
+	typedef Matrix<float> Matrixf;
+}
--- data structures/Vector2D.hpp
+++ data structures/Vector2D.hpp
@@ -3,299 +3,302 @@
 #include <iostream>
 #include <cmath>
 
-template <typename T = double>
-class Vec2 
+namespace lio
 {
-public:
+	template <typename T = double>
+	class Vec2 
+	{
+	public:
 
-	T x, y;
+		T x, y;
 
-	Vec2() : x(0.0), y(0.0) {}
-	template <typename U> Vec2(const U s) : x(s), y(s) {}
-	template <typename U> Vec2(const U x, const U y) : x(x), y(y) {}
-	template <typename U> Vec2(const Vec2<U>& v) : x(v.x), y(v.y) {}
+		Vec2() : x(0.0), y(0.0) {}
+		template <typename U> Vec2(const U s) : x(s), y(s) {}
+		template <typename U> Vec2(const U x, const U y) : x(x), y(y) {}
+		template <typename U> Vec2(const Vec2<U>& v) : x(v.x), y(v.y) {}
 
-	template <typename U>
-	Vec2& operator=(const Vec2<U>& v)
-	{
-		x = v.x;
-		y = v.y;
-		return *this;
-	}
-	template <typename U>
-	Vec2& operator=(const U s)
-	{
-		x = s;
-		y = s;
-		return *this;
-	}
+		template <typename U>
+		Vec2& operator=(const Vec2<U>& v)
+		{
+			x = v.x;
+			y = v.y;
+			return *this;
+		}
+		template <typename U>
+		Vec2& operator=(const U s)
+		{
+			x = s;
+			y = s;
+			return *this;
+		}
 
-	template<typename U>
-	Vec2 operator+(const Vec2<U>& v) const
-	{
-		return Vec2(x + v.x, y + v.y);
-	}
-	template<typename U>
-	Vec2 operator-(const Vec2<U>& v) const
-	{
-		return Vec2(x - v.x, y - v.y);
-	}
-	template<typename U>
-	Vec2 operator*(const Vec2<U>& v) const
-	{
-		return Vec2(x * v.x, y * v.y);
-	}
-	template<typename U>
-	Vec2 operator/(const Vec2<U>& v) const 
-	{
-		return Vec2(x / v.x, y / v.y);
-	}
-	template<typename U>
-	Vec2 operator%(const Vec2<U>& v) const 
-	{
-		return Vec2(x % v.x, y % v.y);
-	}
+		template<typename U>
+		Vec2 operator+(const Vec2<U>& v) const
+		{
+			return Vec2(x + v.x, y + v.y);
+		}
+		template<typename U>
+		Vec2 operator-(const Vec2<U>& v) const
+		{
+			return Vec2(x - v.x, y - v.y);
+		}
+		template<typename U>
+		Vec2 operator*(const Vec2<U>& v) const
+		{
+			return Vec2(x * v.x, y * v.y);
+		}
+		template<typename U>
+		Vec2 operator/(const Vec2<U>& v) const 
+		{
+			return Vec2(x / v.x, y / v.y);
+		}
+		template<typename U>
+		Vec2 operator%(const Vec2<U>& v) const 
+		{
+			return Vec2(x % v.x, y % v.y);
+		}
 
-	template<typename U>
-	Vec2& operator+=(const Vec2<U>& v)
-	{
-		x += v.x;
-		y += v.y;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator-=(const Vec2<U>& v)
-	{
-		x -= v.x;
-		y -= v.y;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator*=(const Vec2<U>& v)
-	{
-		x *= v.x;
-		y *= v.y;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator/=(const Vec2<U>& v)
-	{
-		x /= v.x;
-		y /= v.y;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator%=(const Vec2<U>& v)
-	{
-		x %= v.x;
-		y %= v.y;
-		return *this;
-	}
+		template<typename U>
+		Vec2& operator+=(const Vec2<U>& v)
+		{
+			x += v.x;
+			y += v.y;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator-=(const Vec2<U>& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator*=(const Vec2<U>& v)
+		{
+			x *= v.x;
+			y *= v.y;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator/=(const Vec2<U>& v)
+		{
+			x /= v.x;
+			y /= v.y;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator%=(const Vec2<U>& v)
+		{
+			x %= v.x;
+			y %= v.y;
+			return *this;
+		}
 
-	template<typename U>
-	Vec2 operator+(const U s) const
-	{
-		return Vec2(x + s, y + s);
-	}
-	template<typename U>
-	Vec2 operator-(const U s) const 
-	{
-		return Vec2(x - s, y - s);
-	}
-	template<typename U>
-	Vec2 operator*(const U s) const 
-	{
-		return Vec2(x * s, y * s);
-	}
-	template<typename U>
-	Vec2 operator/(const U s) const
-	{
-		return Vec2(x / s, y / s);
-	}
-	template<typename U>
-	Vec2 operator%(const U s) const
-	{
-		return Vec2(x % s, y % s);
-	}
+		template<typename U>
+		Vec2 operator+(const U s) const
+		{
+			return Vec2(x + s, y + s);
+		}
+		template<typename U>
+		Vec2 operator-(const U s) const 
+		{
+			return Vec2(x - s, y - s);
+		}
+		template<typename U>
+		Vec2 operator*(const U s) const 
+		{
+			return Vec2(x * s, y * s);
+		}
+		template<typename U>
+		Vec2 operator/(const U s) const
+		{
+			return Vec2(x / s, y / s);
+		}
+		template<typename U>
+		Vec2 operator%(const U s) const
+		{
+			return Vec2(x % s, y % s);
+		}
 
-	template<typename U>
-	Vec2& operator+=(const U s)
-	{
-		x += s;
-		y += s;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator-=(const U s)
-	{
-		x -= s;
-		y -= s;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator*=(const U s)
-	{
-		x *= s;
-		y *= s;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator/=(const U s)
-	{
-		x /= s;
-		y /= s;
-		return *this;
-	}
-	template<typename U>
-	Vec2& operator%=(const U s)
-	{
-		x %= s;
-		y %= s;
-		return *this;
-	}
+		template<typename U>
+		Vec2& operator+=(const U s)
+		{
+			x += s;
+			y += s;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator-=(const U s)
+		{
+			x -= s;
+			y -= s;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator*=(const U s)
+		{
+			x *= s;
+			y *= s;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator/=(const U s)
+		{
+			x /= s;
+			y /= s;
+			return *this;
+		}
+		template<typename U>
+		Vec2& operator%=(const U s)
+		{
+			x %= s;
+			y %= s;
+			return *this;
+		}
 
-	Vec2& operator++()
-	{
-		x += 1.0;
-		y += 1.0;
-		return *this;
-	}
-	Vec2& operator--()
-	{
-		x -= 1.0;
-		y -= 1.0;
-		return *this;
-	}
-	Vec2 operator++(int)
-	{
-		Vec2 tmp(x, y);
-		x += 1.0;
-		y += 1.0;
-		return tmp;
-	}
-	Vec2 operator--(int)
-	{
-		Vec2 tmp(x, y);
-		x -= 1.0;
-		y -= 1.0;
-		return tmp;
-	}
+		Vec2& operator++()
+		{
+			x += 1.0;
+			y += 1.0;
+			return *this;
+		}
+		Vec2& operator--()
+		{
+			x -= 1.0;
+			y -= 1.0;
+			return *this;
+		}
+		Vec2 operator++(int)
+		{
+			Vec2 tmp(x, y);
+			x += 1.0;
+			y += 1.0;
+			return tmp;
+		}
+		Vec2 operator--(int)
+		{
+			Vec2 tmp(x, y);
+			x -= 1.0;
+			y -= 1.0;
+			return tmp;
+		}
 
-	template <typename U>
-	friend bool operator==(const Vec2& v1, const Vec2<U>& v2)
-	{
-		return v1.x == v2.x && v1.y == v2.y;
-	}
-	template <typename U>
-	friend bool operator!=(const Vec2& v1, const Vec2<U>& v2)
-	{
-		return !(v1.x == v2.x && v1.y == v2.y);
-	}
+		template <typename U>
+		friend bool operator==(const Vec2& v1, const Vec2<U>& v2)
+		{
+			return v1.x == v2.x && v1.y == v2.y;
+		}
+		template <typename U>
+		friend bool operator!=(const Vec2& v1, const Vec2<U>& v2)
+		{
+			return !(v1.x == v2.x && v1.y == v2.y);
+		}
 
-	friend std::ostream& operator<<(std::ostream& os, const Vec2& v)
-	{
-		os << v.x << ", " << v.y;
-		return os;
-	}
-	friend std::istream& operator>>(std::istream& is, Vec2& v)
-	{
-		is >> v.x >> v.y;
-		return is;
-	}
+		friend std::ostream& operator<<(std::ostream& os, const Vec2& v)
+		{
+			os << v.x << ", " << v.y;
+			return os;
+		}
+		friend std::istream& operator>>(std::istream& is, Vec2& v)
+		{
+			is >> v.x >> v.y;
+			return is;
+		}
 
-	template<typename U>
-	void Set(U x, U y)
-	{
-		this->x = x;
-		this->y = y;
-	}
+		template<typename U>
+		void Set(U x, U y)
+		{
+			this->x = x;
+			this->y = y;
+		}
 
-	template<typename S = T, typename U>
-	Vec2<S> Rotated(U theta) const
-	{
-		double c = cos(theta);
-		double s = sin(theta);
-		S tx = static_cast<S>(x * c - y * s);
-		S ty = static_cast<S>(x * s + y * c);
+		template<typename S = T, typename U>
+		Vec2<S> Rotated(U theta) const
+		{
+			double c = cos(theta);
+			double s = sin(theta);
+			S tx = static_cast<S>(x * c - y * s);
+			S ty = static_cast<S>(x * s + y * c);
 
-		return Vec2<S>(tx, ty);
-	}
-	template<typename U>
-	Vec2& Rotate(U theta)
-	{
-		double c = cos(theta);
-		double s = sin(theta);
-		T tx = static_cast<T>(x * c - y * s);
-		T ty = static_cast<T>(x * s + y * c);
-		x = tx;
-		y = ty;
+			return Vec2<S>(tx, ty);
+		}
+		template<typename U>
+		Vec2& Rotate(U theta)
+		{
+			double c = cos(theta);
+			double s = sin(theta);
+			T tx = static_cast<T>(x * c - y * s);
+			T ty = static_cast<T>(x * s + y * c);
+			x = tx;
+			y = ty;
 
-		return *this;
-	}
+			return *this;
+		}
 
-	Vec2<double> Normalized() const
-	{
-		if (Magnitude() == 0) return *this;
-		return Vec2<double>(this->Cast<double>() / Magnitude());
-	}
-	Vec2& Normalize()
-	{
-		if (Magnitude() == 0) return *this;
-		*this *= (1.0 / Magnitude());
-		return *this;
-	}
+		Vec2<double> Normalized() const
+		{
+			if (Magnitude() == 0) return *this;
+			return Vec2<double>(this->Cast<double>() / Magnitude());
+		}
+		Vec2& Normalize()
+		{
+			if (Magnitude() == 0) return *this;
+			*this *= (1.0 / Magnitude());
+			return *this;
+		}
 
-	Vec2 Ortho() const
-	{
-		return Vec2(y, -x);
-	}
+		Vec2 Ortho() const
+		{
+			return Vec2(y, -x);
+		}
 
-	template <typename U>
-	double Distance(const Vec2<U>& v) const
-	{
-		Vec2 d(v.x - x, v.y - y);
-		return d.Magnitude();
-	}
+		template <typename U>
+		double Distance(const Vec2<U>& v) const
+		{
+			Vec2 d(v.x - x, v.y - y);
+			return d.Magnitude();
+		}
 
-	double SqrMagnitude() const
-	{
-		return x * x + y * y;
-	}
-	double Magnitude() const
-	{
-		return std::sqrt(SqrMagnitude());
-	}
+		double SqrMagnitude() const
+		{
+			return x * x + y * y;
+		}
+		double Magnitude() const
+		{
+			return std::sqrt(SqrMagnitude());
+		}
 
-	Vec2 Abs() const
-	{
-		return Vec2(x < 0 ? x * -1 : x, y < 0 ? y * -1 : y);
-	}
+		Vec2 Abs() const
+		{
+			return Vec2(x < 0 ? x * -1 : x, y < 0 ? y * -1 : y);
+		}
 
-	template <typename U>
-	double Dot(const Vec2<U>& v)
-	{
-		return x * v.x + y * v.y;
-	}
-	template <typename U>
-	double Cross(const Vec2<U>& v)
-	{
-		return (x * v.x) - (y * v.y);
-	}
+		template <typename U>
+		double Dot(const Vec2<U>& v)
+		{
+			return x * v.x + y * v.y;
+		}
+		template <typename U>
+		double Cross(const Vec2<U>& v)
+		{
+			return (x * v.x) - (y * v.y);
+		}
 
-	static Vec2 One()	{ return Vec2(1.0, 1.0); }
-	static Vec2 Zero()	{ return Vec2(0.0, 0.0); }
-	static Vec2 Up()	{ return Vec2(0.0, 1.0); }
-	static Vec2 Down()	{ return Vec2(0.0, -1.0); }
-	static Vec2 Left()	{ return Vec2(-1.0, 0.0); }
-	static Vec2 Right()	{ return Vec2(1.0, 0.0); }
+		static Vec2 One()	{ return Vec2(1.0, 1.0); }
+		static Vec2 Zero()	{ return Vec2(0.0, 0.0); }
+		static Vec2 Up()	{ return Vec2(0.0, 1.0); }
+		static Vec2 Down()	{ return Vec2(0.0, -1.0); }
+		static Vec2 Left()	{ return Vec2(-1.0, 0.0); }
+		static Vec2 Right()	{ return Vec2(1.0, 0.0); }
 
-	template <typename U>
-	Vec2<U> Cast() const
-	{
-		return Vec2<U>(x, y);
-	}
-};
+		template <typename U>
+		Vec2<U> Cast() const
+		{
+			return Vec2<U>(x, y);
+		}
+	};
 
-typedef Vec2<double> Vec2d;
-typedef Vec2<float> Vec2f;
-typedef Vec2<int> Vec2i;
+	typedef Vec2<double> Vec2d;
+	typedef Vec2<float> Vec2f;
+	typedef Vec2<int> Vec2i;
+}
