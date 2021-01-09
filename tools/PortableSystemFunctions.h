b8899fdd30a962b4fe1992a9b21d96263b2e1126
LioQing <s2010157@gmail.com>
2021-01-09 23:23:02 +0800

    Update PortableSystemFunctions.h

--- tools/PortableSystemFunctions.h
+++ tools/PortableSystemFunctions.h
@@ -1,4 +1,11 @@
 #include <stdint.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+	#include <windows.h>
+#else
+	#include <unistd.h>
+#endif
 
 // clear
 void ClearScreen()
@@ -11,12 +18,6 @@ void ClearScreen()
 }
 
 // sleep
-#ifdef _WIN32
-	#include <windows.h>
-#else
-	#include <unistd.h>
-#endif
-
 void Sleep(uint32_t ms)
 {
 #ifdef _WIN32
